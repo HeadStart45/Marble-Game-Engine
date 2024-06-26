302bf2c1f40efd22ddcad56afa2f857b89cce046
HeadStart45 <60329415+HeadStart45@users.noreply.github.com>
2024-06-26 12:54:45 +1200

    Added window to application

--- Marble/src/Marble/Application.cpp
+++ Marble/src/Marble/Application.cpp
@@ -4,10 +4,14 @@
 #include "Marble/Events/MouseEvent.h"
 #include "Marble/Log.h"
 
+///Remember to set GLFW project>CodeGeneration>Runtime library to /MT when you remake
+
+
 namespace Marble
 {
 	Application::Application()
 	{
+		m_Window = std::unique_ptr<Window>(Window::Create());
 	}
 
 	Application::~Application()
@@ -15,12 +19,10 @@ namespace Marble
 	}
 	void Application::Run()
 	{
-		MouseButtonPressedEvent e(2);
-		MB_CORE_LOG(e.ToString());
-		
-		
-		
-		while (true);
+		while (m_Running)
+		{
+			m_Window->OnUpdate();
+		}
 	}
 }
 
--- Marble/src/Marble/Application.h
+++ Marble/src/Marble/Application.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "Core.h"
-
+#include "Window.h"
 
 namespace Marble
 {
@@ -12,6 +12,9 @@ namespace Marble
 		~Application();
 
 		void Run();
+	private:
+		std::shared_ptr<Window> m_Window;
+		bool m_Running = true;
 	};
 
 	//Defined in CLIENT
